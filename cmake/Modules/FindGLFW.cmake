0b8874919c49621b4677f52b64dd5706e2f0c623
Adrien CASSAGNE <adrien.cassagne@inria.fr>
2017-07-09 15:10:54 +0000

    Do not crash when the GLFW lib is not found.

--- cmake/Modules/FindGLFW.cmake
+++ cmake/Modules/FindGLFW.cmake
@@ -138,28 +138,28 @@ else ()
     else ()
         # (*)NIX
         
-        find_package(Threads REQUIRED)
+        find_package(Threads)
 
-        find_package(X11 REQUIRED)
+        find_package(X11)
         
         if(NOT X11_Xrandr_FOUND)
-            message(FATAL_ERROR "Xrandr library not found - required for GLFW")
+            message(STATUS "Xrandr library not found - required for GLFW")
         endif()
 
         if(NOT X11_xf86vmode_FOUND)
-            message(FATAL_ERROR "xf86vmode library not found - required for GLFW")
+            message(STATUS "xf86vmode library not found - required for GLFW")
         endif()
 
         if(NOT X11_Xcursor_FOUND)
-            message(FATAL_ERROR "Xcursor library not found - required for GLFW")
+            message(STATUS "Xcursor library not found - required for GLFW")
         endif()
 
         if(NOT X11_Xinerama_FOUND)
-            message(FATAL_ERROR "Xinerama library not found - required for GLFW")
+            message(STATUS "Xinerama library not found - required for GLFW")
         endif()
 
         if(NOT X11_Xi_FOUND)
-            message(FATAL_ERROR "Xi library not found - required for GLFW")
+            message(STATUS "Xi library not found - required for GLFW")
         endif()
 
         list(APPEND GLFW_x11_LIBRARY "${X11_Xrandr_LIB}" "${X11_Xxf86vm_LIB}" "${X11_Xcursor_LIB}" "${X11_Xinerama_LIB}" "${X11_Xi_LIB}" "${X11_LIBRARIES}" "${CMAKE_THREAD_LIBS_INIT}" -lrt -ldl)
