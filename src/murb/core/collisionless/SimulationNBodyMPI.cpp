3928ae5c8e3bf83afbade70dd6b51ed2217a5a19
cameo <cameo@cines.fr>
2021-09-09 09:13:31 +0200

    IMPROVE MPI C bindings

--- src/murb/core/collisionless/SimulationNBodyMPI.cpp
+++ src/murb/core/collisionless/SimulationNBodyMPI.cpp
@@ -66,67 +66,67 @@ void SimulationNBodyMPI<T>::init()
 	Bodies<T> *bBuffs[] = {&this->MPIBodiesBuffers[0], &this->MPIBodiesBuffers[1]};
 
 	// send number of bodies
-	this->MPIRequests[0][0] = MPI_Send_init(&bBuffs[0]->n,            1, MPI_LONG, MPINextRank, 0);
-	this->MPIRequests[1][0] = MPI_Send_init(&bBuffs[1]->n,            1, MPI_LONG, MPINextRank, 1);
+	MPI_Send_init(&bBuffs[0]->n,            1, MPI_LONG, MPINextRank, 0), MPI_COMM_WORLD, &this->MPIRequests[0][0];
+	MPI_Send_init(&bBuffs[1]->n,            1, MPI_LONG, MPINextRank, 1), MPI_COMM_WORLD, &this->MPIRequests[1][0];
 	// receive number of bodies
-	this->MPIRequests[0][1] = MPI_Recv_init(&bBuffs[1]->n,            1, MPI_LONG, MPIPrevRank, 0);
-	this->MPIRequests[1][1] = MPI_Recv_init(&bBuffs[0]->n,            1, MPI_LONG, MPIPrevRank, 1);
+	MPI_Recv_init(&bBuffs[1]->n,            1, MPI_LONG, MPIPrevRank, 0), MPI_COMM_WORLD, &this->MPIRequests[0][1];
+	MPI_Recv_init(&bBuffs[0]->n,            1, MPI_LONG, MPIPrevRank, 1), MPI_COMM_WORLD, &this->MPIRequests[1][1];
 
 	// send masses
-	this->MPIRequests[0][2] = MPI_Send_init(bBuffs[0]->masses,        n, MPIType,  MPINextRank, 2);
-	this->MPIRequests[1][2] = MPI_Send_init(bBuffs[1]->masses,        n, MPIType,  MPINextRank, 3);
+	MPI_Send_init(bBuffs[0]->masses,        n, MPIType,  MPINextRank, 2), MPI_COMM_WORLD, &this->MPIRequests[0][2];
+	MPI_Send_init(bBuffs[1]->masses,        n, MPIType,  MPINextRank, 3), MPI_COMM_WORLD, &this->MPIRequests[1][2];
 	// receive masses
-	this->MPIRequests[0][3] = MPI_Recv_init(bBuffs[1]->masses,        n, MPIType,  MPIPrevRank, 2);
-	this->MPIRequests[1][3] = MPI_Recv_init(bBuffs[0]->masses,        n, MPIType,  MPIPrevRank, 3);
+	MPI_Recv_init(bBuffs[1]->masses,        n, MPIType,  MPIPrevRank, 2), MPI_COMM_WORLD, &this->MPIRequests[0][3];
+	MPI_Recv_init(bBuffs[0]->masses,        n, MPIType,  MPIPrevRank, 3), MPI_COMM_WORLD, &this->MPIRequests[1][3];
 
 	// send radiuses
-	this->MPIRequests[0][4] = MPI_Send_init(bBuffs[0]->radiuses,      n, MPIType,  MPINextRank, 4);
-	this->MPIRequests[1][4] = MPI_Send_init(bBuffs[1]->radiuses,      n, MPIType,  MPINextRank, 5);
+	MPI_Send_init(bBuffs[0]->radiuses,      n, MPIType,  MPINextRank, 4), MPI_COMM_WORLD, &this->MPIRequests[0][4];
+	MPI_Send_init(bBuffs[1]->radiuses,      n, MPIType,  MPINextRank, 5), MPI_COMM_WORLD, &this->MPIRequests[1][4];
 	// receive radiuses
-	this->MPIRequests[0][5] = MPI_Recv_init(bBuffs[1]->radiuses,      n, MPIType,  MPIPrevRank, 4);
-	this->MPIRequests[1][5] = MPI_Recv_init(bBuffs[0]->radiuses,      n, MPIType,  MPIPrevRank, 5);
+	MPI_Recv_init(bBuffs[1]->radiuses,      n, MPIType,  MPIPrevRank, 4), MPI_COMM_WORLD, &this->MPIRequests[0][5];
+	MPI_Recv_init(bBuffs[0]->radiuses,      n, MPIType,  MPIPrevRank, 5), MPI_COMM_WORLD, &this->MPIRequests[1][5];
 
 	// send positions.x
-	this->MPIRequests[0][6] = MPI_Send_init(bBuffs[0]->positions.x,   n, MPIType,  MPINextRank, 6);
-	this->MPIRequests[1][6] = MPI_Send_init(bBuffs[1]->positions.x,   n, MPIType,  MPINextRank, 7);
+	MPI_Send_init(bBuffs[0]->positions.x,   n, MPIType,  MPINextRank, 6, MPI_COMM_WORLD, &this->MPIRequests[0][6]);
+	MPI_Send_init(bBuffs[1]->positions.x,   n, MPIType,  MPINextRank, 7, MPI_COMM_WORLD, &this->MPIRequests[1][6]);
 	// receive positions.x
-	this->MPIRequests[0][7] = MPI_Recv_init(bBuffs[1]->positions.x,   n, MPIType,  MPIPrevRank, 6);
-	this->MPIRequests[1][7] = MPI_Recv_init(bBuffs[0]->positions.x,   n, MPIType,  MPIPrevRank, 7);
+	MPI_Recv_init(bBuffs[1]->positions.x,   n, MPIType,  MPIPrevRank, 6, MPI_COMM_WORLD, &this->MPIRequests[0][7]);
+	MPI_Recv_init(bBuffs[0]->positions.x,   n, MPIType,  MPIPrevRank, 7, MPI_COMM_WORLD, &this->MPIRequests[1][7]);
 
 	// send positions.y
-	this->MPIRequests[0][8] = MPI_Send_init(bBuffs[0]->positions.y,   n, MPIType,  MPINextRank, 8);
-	this->MPIRequests[1][8] = MPI_Send_init(bBuffs[1]->positions.y,   n, MPIType,  MPINextRank, 9);
+	MPI_Send_init(bBuffs[0]->positions.y,   n, MPIType,  MPINextRank, 8, MPI_COMM_WORLD, &this->MPIRequests[0][8]);
+	MPI_Send_init(bBuffs[1]->positions.y,   n, MPIType,  MPINextRank, 9, MPI_COMM_WORLD, &this->MPIRequests[1][8]);
 	// receive positions.y
-	this->MPIRequests[0][9] = MPI_Recv_init(bBuffs[1]->positions.y,   n, MPIType,  MPIPrevRank, 8);
-	this->MPIRequests[1][9] = MPI_Recv_init(bBuffs[0]->positions.y,   n, MPIType,  MPIPrevRank, 9);
+	MPI_Recv_init(bBuffs[1]->positions.y,   n, MPIType,  MPIPrevRank, 8, MPI_COMM_WORLD, &this->MPIRequests[0][9]);
+	MPI_Recv_init(bBuffs[0]->positions.y,   n, MPIType,  MPIPrevRank, 9, MPI_COMM_WORLD, &this->MPIRequests[1][9]);
 
 	// send positions.z
-	this->MPIRequests[0][10] = MPI_Send_init(bBuffs[0]->positions.z,  n, MPIType, MPINextRank, 10);
-	this->MPIRequests[1][10] = MPI_Send_init(bBuffs[1]->positions.z,  n, MPIType, MPINextRank, 11);
+	MPI_Send_init(bBuffs[0]->positions.z,  n, MPIType, MPINextRank, 10, MPI_COMM_WORLD, &this->MPIRequests[0][10]);
+	MPI_Send_init(bBuffs[1]->positions.z,  n, MPIType, MPINextRank, 11, MPI_COMM_WORLD, &this->MPIRequests[1][10]);
 	// receive positions.z
-	this->MPIRequests[0][11] = MPI_Recv_init(bBuffs[1]->positions.z,  n, MPIType, MPIPrevRank, 10);
-	this->MPIRequests[1][11] = MPI_Recv_init(bBuffs[0]->positions.z,  n, MPIType, MPIPrevRank, 11);
+	MPI_Recv_init(bBuffs[1]->positions.z,  n, MPIType, MPIPrevRank, 10, MPI_COMM_WORLD, &this->MPIRequests[0][11]);
+	MPI_Recv_init(bBuffs[0]->positions.z,  n, MPIType, MPIPrevRank, 11, MPI_COMM_WORLD, &this->MPIRequests[1][11]);
 
 	// send velocities.x
-	this->MPIRequests[0][12] = MPI_Send_init(bBuffs[0]->velocities.x, n, MPIType, MPINextRank, 12);
-	this->MPIRequests[1][12] = MPI_Send_init(bBuffs[1]->velocities.x, n, MPIType, MPINextRank, 13);
+	MPI_Send_init(bBuffs[0]->velocities.x, n, MPIType, MPINextRank, 12, MPI_COMM_WORLD, &this->MPIRequests[0][12]);
+	MPI_Send_init(bBuffs[1]->velocities.x, n, MPIType, MPINextRank, 13, MPI_COMM_WORLD, &this->MPIRequests[1][12]);
 	// receive velocities.x
-	this->MPIRequests[0][13] = MPI_Recv_init(bBuffs[1]->velocities.x, n, MPIType, MPIPrevRank, 12);
-	this->MPIRequests[1][13] = MPI_Recv_init(bBuffs[0]->velocities.x, n, MPIType, MPIPrevRank, 13);
+	MPI_Recv_init(bBuffs[1]->velocities.x, n, MPIType, MPIPrevRank, 12, MPI_COMM_WORLD, &this->MPIRequests[0][13]);
+	MPI_Recv_init(bBuffs[0]->velocities.x, n, MPIType, MPIPrevRank, 13, MPI_COMM_WORLD, &this->MPIRequests[1][13]);
 
 	// send velocities.y
-	this->MPIRequests[0][14] = MPI_Send_init(bBuffs[0]->velocities.y, n, MPIType, MPINextRank, 14);
-	this->MPIRequests[1][14] = MPI_Send_init(bBuffs[1]->velocities.y, n, MPIType, MPINextRank, 15);
+	MPI_Send_init(bBuffs[0]->velocities.y, n, MPIType, MPINextRank, 14, MPI_COMM_WORLD, &this->MPIRequests[0][14]);
+	MPI_Send_init(bBuffs[1]->velocities.y, n, MPIType, MPINextRank, 15, MPI_COMM_WORLD, &this->MPIRequests[1][14]);
 	// receive velocities.y
-	this->MPIRequests[0][15] = MPI_Recv_init(bBuffs[1]->velocities.y, n, MPIType, MPIPrevRank, 14);
-	this->MPIRequests[1][15] = MPI_Recv_init(bBuffs[0]->velocities.y, n, MPIType, MPIPrevRank, 15);
+	MPI_Recv_init(bBuffs[1]->velocities.y, n, MPIType, MPIPrevRank, 14, MPI_COMM_WORLD, &this->MPIRequests[0][15]);
+	MPI_Recv_init(bBuffs[0]->velocities.y, n, MPIType, MPIPrevRank, 15, MPI_COMM_WORLD, &this->MPIRequests[1][15]);
 
 	// send velocities.z
-	this->MPIRequests[0][16] = MPI_Send_init(bBuffs[0]->velocities.z, n, MPIType, MPINextRank, 16);
-	this->MPIRequests[1][16] = MPI_Send_init(bBuffs[1]->velocities.z, n, MPIType, MPINextRank, 17);
+	MPI_Send_init(bBuffs[0]->velocities.z, n, MPIType, MPINextRank, 16, MPI_COMM_WORLD, &this->MPIRequests[0][16]);
+	MPI_Send_init(bBuffs[1]->velocities.z, n, MPIType, MPINextRank, 17, MPI_COMM_WORLD, &this->MPIRequests[1][16]);
 	// receive velocities.z
-	this->MPIRequests[0][17] = MPI_Recv_init(bBuffs[1]->velocities.z, n, MPIType, MPIPrevRank, 16);
-	this->MPIRequests[1][17] = MPI_Recv_init(bBuffs[0]->velocities.z, n, MPIType, MPIPrevRank, 17);
+	MPI_Recv_init(bBuffs[1]->velocities.z, n, MPIType, MPIPrevRank, 16, MPI_COMM_WORLD, &this->MPIRequests[0][17]);
+	MPI_Recv_init(bBuffs[0]->velocities.z, n, MPIType, MPIPrevRank, 17, MPI_COMM_WORLD, &this->MPIRequests[1][17]);
 }
 
 template <typename T>
